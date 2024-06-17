bf2715e5d201537af888d313ae23c17053d65048
christianj16 <christianjansen16@gmail.com>
2024-06-16 21:40:52 -0700

    finished blog app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AllPosts from './pages/AllPosts'
+import SinglePost from './pages/SinglePost'
+import Form from './pages/Form'
+
+const apiURL = 'https://blog-app-backend-4dc308270c08.herokuapp.com'
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const getBlogs = async () => {
+    const response = await fetch(apiURL + '/blog_app/')
+    const data = await response.json()
+    console.log(data)
+    setPosts(data)
+  }
+
+  const handleFormSubmission = async (data, type) => {
+    if(type === 'new') {
+      const response = await fetch(`${apiURL}/blog_app/`, {
+        method: 'post',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+      })
+      getBlogs()
+    } else {
+      const response = await fetch(`${apiURL}/blog_app/${data.id}/`, {
+        method: 'put',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+      })
+      getBlogs()
+    }
+  }
+
+  const deleteBlog = async (id) => {
+    const response = await fetch(`${apiURL}/blog_app/${id}/`,
+      {
+        method: 'delete'
+      })
+      getBlogs()
+  }
+
+  useEffect(() => {
+    getBlogs()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Blogs</h1>
+      <Routes>
+        <Route
+          exact 
+          path= "/"
+          element={<AllPosts posts={posts} deleteBlog={deleteBlog} />}
+        />
+        <Route
+          exact 
+          path= "/post/:id"
+          element={<SinglePost posts={posts}/>}
+        />
+        <Route
+          exact 
+          path= "/new"
+          element={<Form posts={posts} handleSubmit={handleFormSubmission} buttonLabel='Add Blog'
+          formType='new' />}
+        />
+        <Route
+          exact 
+          path= "/edit/:id"
+          element={<Form posts={posts} handleSubmit={handleFormSubmission}
+          buttonLebel='Edit Blog' formType='edit'/>}
+        />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
